a7802226ef5c24390e977e8d3f40e20235192169
bonkab.koo <bonkab.koo@lge.com>
2024-11-10 15:50:53 +0900

    update get-port

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const fs = require('node:fs/promises');
 const { createReadStream } = require('node:fs');
 const http = require('http');
-const FormData = require('form-data'); // FormData 라이브러리 사용
+const FormData = require('form-data');
+const getPort = require('get-port');
 
 async function dynamicImport(module) {
   return await import(module);
 }
 
-async function startServer(logfile, port, flags, postUrl, key, homeyId, appId, retryCount = 0, maxRetries = 10) {
+async function startServer(logfile, flags, postUrl, key, homeyId, appId, startPort = 8008) {
   const { hookStd } = await import('hook-std');
   const { default: fetch } = await dynamicImport('node-fetch');
   const fh = await fs.open(logfile, flags);
+
+  // 지정된 시작 포트부터 +100까지 포트 범위를 설정
+  const port = await getPort({ port: getPort.makeRange(startPort, startPort + 100) });
 
   return new Promise((resolve, reject) => {
     const server = http.createServer(async (req, res) => {
@@ -37,22 +41,13 @@ async function startServer(logfile, port, flags, postUrl, key, homeyId, appId, r
       }
     });
 
-    server.on('error', async (error) => {
-      if (error.code === 'EADDRINUSE') {
-        if (retryCount < maxRetries) {
-          console.log(`Port ${port} is in use, trying port ${port + 1}`);
-          resolve(startServer(logfile, port + 1, flags, postUrl, key, homeyId, appId, retryCount + 1, maxRetries));
-        } else {
-          reject(new Error(`Failed to start server after ${maxRetries} attempts. Please check port availability.`));
-        }
-      } else {
-        reject(error);
-      }
-    });
-
     server.listen(port, () => {
       console.log(`Server is listening on port ${port}`);
-      resolve(port); // 성공적으로 포트가 열렸을 때 해당 포트를 반환
+      resolve(port);
+    });
+
+    server.on('error', (error) => {
+      reject(error);
     });
 
     hookStd({ silent: false }, output => { fh.write(output) });
@@ -62,12 +57,12 @@ async function startServer(logfile, port, flags, postUrl, key, homeyId, appId, r
 module.exports = async (config) => {
   const port = await startServer(
     config.logfile || '/userdata/std.log',
-    config.port || 8008,
     config.flags || 'a',
     config.postUrl || "",
     config.key || "",
     config.homeyId || "unknown",
-    config.appId || "unknown"
+    config.appId || "unknown",
+    config.port || 8008 // 기본 시작 포트를 config에서 설정 가능
   );
   console.log(`Server successfully started on port: ${port}`);
   return port;
